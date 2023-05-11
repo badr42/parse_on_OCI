ff64e17512b944faa5f74613cfd85a38dc92da69
badr42 <badr42@gmail.com>
2023-05-12 02:01:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,10 +54,9 @@ resource "oci_core_instance" "instance" {
 
   provisioner "remote-exec" {
     inline = [
-      "export pass=${var.Parse_pass}",
       "sudo su - ",
       "wget https://raw.githubusercontent.com/badr42/parse_on_OCI/main/install.sh",
-      "bash install.sh ${var.Parse_pass}",
+      "sh install.sh ${var.Parse_pass}",
       //"wget -qO - 'https://raw.githubusercontent.com/badr42/parse_on_OCI/main/install.sh' | bash -s ${var.Parse_pass}",
     ]
   }
